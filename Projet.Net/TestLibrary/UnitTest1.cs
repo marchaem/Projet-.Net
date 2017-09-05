6e407eea01abf8b6d08c9a1c8eda3010ed77ff16
ensimag <ensimag@ENSIPC454>
2017-09-05 14:27:34 +0200

    test unitaires

---
+++ Projet.Net/TestLibrary/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace TestLibrary
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Debug.WriteLine("hello");
+        }
+    }
+}
