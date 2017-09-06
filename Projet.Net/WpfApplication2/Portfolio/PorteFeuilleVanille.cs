731e01eb9ec3152858450afafaeac77290eb5ca8
ensimag <ensimag@ENSIPC455>
2017-09-06 15:34:28 +0200

    Reballancement

--- Projet.Net/WpfApplication2/Portfolio/PorteFeuilleVanille.cs
+++ Projet.Net/WpfApplication2/Portfolio/PorteFeuilleVanille.cs
@@ -4,20 +4,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfApplication2.Options;
 
 namespace WpfApplication2.Portfolio
 {
     public class PorteFeuilleVanille : Portefeuille
     {
-        private Share action { get; set; }
-        private uint nombreAction { get; set; }
-        private double thuneDansLeSansRisque { get; set; }
+        private OptionVanille option { get; set; }
 
-        public PorteFeuilleVanille(Share action, uint nombreAction, double thune)
+
+// proportions est un dictionnaire qui à une date donnée associe delta et (prix - delta)
+        private Dictionary<System.DateTime, double[]> proportions;
+
+        public PorteFeuilleVanille(OptionVanille option, Dictionary<System.DateTime,double[]> proportions)
         {
-            this.action = action;
-            this.nombreAction = nombreAction;
-            this.thuneDansLeSansRisque = thune;
+            this.option = option;
+            this.proportions = proportions;
+        }
+        public void actualisationPortef(DateTime date,double spot,double volatility)
+        {
+            double nbAction = option.calculDeltaVanille(date, 365, spot,  volatility);
+            double thuneSansrisque = 1; 
+        }
+        
+        public double pricePortefeuille(System.DateTime datePrec, double prixSousJ ,double r)
+        {
+            double[] proportion = new double[] { 0, 0 };
+            this.proportions.TryGetValue(datePrec, out proportion);
+            return proportion[0] * prixSousJ + proportion[1] * (1 + r); 
+
+
+
         }
     }
 }
