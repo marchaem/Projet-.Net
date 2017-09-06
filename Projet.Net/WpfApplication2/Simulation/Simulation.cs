0ad37641b65d6cb46d634f6e32e051566e30d1b8
ensimag <ensimag@ENSIPC453>
2017-09-06 16:01:57 +0200

    Debut simulation

---
+++ Projet.Net/WpfApplication2/Simulation/Simulation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApplication2.Parametres;
+
+namespace WpfApplication2.Simulation
+{
+    class Simulation
+    {
+        private Entrees param;
+
+        public Simulation(Parametres param)
+        {
+            this.param = param;
+        }
+
+        public void Lancer()
+        {
+
+        }
+
+    }
+}
