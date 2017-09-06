51765b3499ac4b4ae294ebbca651bc7ea5a7ac29
thomas <thomas.kerboul@hotmail.fr>
2017-09-06 11:37:41 +0200

    j anule l eur conneri ^^

--- Projet.Net/WpfApplication2/MainWindow.xaml.cs
+++ Projet.Net/WpfApplication2/MainWindow.xaml.cs
@@ -43,6 +43,7 @@ namespace WpfApplication2
             }
 
             Console.WriteLine("debut de la generation de data");
+
             var hey = new List<DataFeed>();
             DateTime abc = new DateTime(2010, 1, 1);
             DateTime abc2 = new DateTime(2009,1,1);
