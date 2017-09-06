368ec09852ba022c31863d7f7cf821c5faefcbf8
ensimag <ensimag@ENSIPC453>
2017-09-06 13:07:55 +0200

    Nettoyage main

--- Projet.Net/WpfApplication2/MainWindow.xaml.cs
+++ Projet.Net/WpfApplication2/MainWindow.xaml.cs
@@ -25,62 +25,20 @@ namespace WpfApplication2
     {
         public MainWindow()
         {
-            Console.WriteLine("Démarrer");
-            Console.ReadLine();
-
-            /*Recuperation des donnes*/
-
-            DataHistoriqueDataContext asdc = new DataHistoriqueDataContext();
-
-            List<string> reqId = asdc.HistoricalShareValues.Select(el => el.id).Distinct().ToList();
-            List<System.DateTime> reqDate = asdc.HistoricalShareValues.Select(el => el.date).Distinct().ToList();
-            List<decimal> reqValue = asdc.HistoricalShareValues.Select(el => el.value).Distinct().ToList();
-
-            /*Calcul des donnees simulées*/
-
-            Console.WriteLine("debut de la generation de data");
-
-            var hey = new List<DataFeed>();
-            DateTime abc = new DateTime(2010, 1, 1);
-            DateTime abc2 = new DateTime(2009, 1, 1);
-            Share action1 = new Share("accor", "accordId");
-            VanillaCall vanille1 = new VanillaCall("Accor", action1, abc, 10);
-            IDataFeedProvider data = new SimulatedDataFeedProvider();
-            hey = data.GetDataFeed(vanille1, abc2);
-            Console.WriteLine(hey[0].Date);
-            Console.WriteLine(hey[0].PriceList.ToString());
-            decimal a = 10;
-            hey[50].PriceList.TryGetValue("accorId", out a);
-            Console.WriteLine(a);
-            Console.WriteLine("fin");
-
-            /*Fin données simulées*/
-
-            Console.WriteLine("Il y a  " + reqValue.Count + " valeurs");
-
-            /*Recuperation liste*/
-
-            List<Share> listeActions = new List<Share>();
-            foreach(string nom in reqId) {
-                listeActions.Add(new Share(nom, nom));
-                Console.WriteLine("Action = " + nom);
-            }
 
             /*Donnees a demander a l'utilisateur*/
 
-            DateTime debut = new DateTime(2015, 01, 01);
-
-            DateTime maturite = new DateTime(2015, 10, 10);
-            double strike = 10.0;
-
+            DateTime debut = new DateTime(2009, 01, 01);
+            DateTime maturite = new DateTime(2010, 10, 10);
+            double strike = 0.0;
             bool simule = true;
 
             /*On prend par exemple la premiere action de la liste*/
-            Share action = listeActions[0];
-            Console.WriteLine("Action selectionné " + action.Name);
+            Share action = new Share("accor", "accordId");
+            Console.WriteLine("Action selectionnée " + action.Name);
 
-            DateTime debutEstimation;
-            DateTime finEstimation;
+            DateTime debutEstimation = new DateTime(2009,1,1);
+            DateTime finEstimation = new DateTime(2010,1,1);
 
             /*Fin entree des donnees*/
 
@@ -89,9 +47,18 @@ namespace WpfApplication2
             VanillaCall vanille = new VanillaCall("option", action, maturite, strike);
 
             /*Calcul de la valeur du payoff*/
+            var dataFeedCalc = new List<DataFeed>();
+            double payoff;
 
-            double payoff = vanille.GetPayoff(hey[0].PriceList);
-            Console.WriteLine("La payoff de l'option vaut " + payoff);
+            if (simule)
+            {
+                IDataFeedProvider data = new SimulatedDataFeedProvider();
+                dataFeedCalc = data.GetDataFeed(vanille, debut);
+                payoff = vanille.GetPayoff(dataFeedCalc[200].PriceList);
+                Console.WriteLine("La payoff de l'option vaut " + payoff);
+            }
+
+
 
             //vanille.GetPayoff();
 
@@ -100,6 +67,26 @@ namespace WpfApplication2
 
             //InitializeComponent();
 
+        }
+
+        public void getDatahist()
+        {
+            /*Recuperation des donnes*/
+
+            DataHistoriqueDataContext asdc = new DataHistoriqueDataContext();
+
+            List<string> reqId = asdc.HistoricalShareValues.Select(el => el.id).Distinct().ToList();
+            List<System.DateTime> reqDate = asdc.HistoricalShareValues.Select(el => el.date).Distinct().ToList();
+            List<decimal> reqValue = asdc.HistoricalShareValues.Select(el => el.value).Distinct().ToList();
+
+            Console.WriteLine("Il y a  " + reqValue.Count + " valeurs");
+
+            List<Share> listeActions = new List<Share>();
+            foreach (string nom in reqId)
+            {
+                listeActions.Add(new Share(nom, nom));
+                Console.WriteLine("Action = " + nom);
+            }
 
             for (int i = 0; i < reqId.Count; ++i)
             {
@@ -108,6 +95,25 @@ namespace WpfApplication2
                 Console.WriteLine("Value: " + reqValue[i]);
             }
 
+        }
+
+        public void getDataSimul()
+        {
+            Console.WriteLine("debut de la generation de data");
+
+            var dataFeedCalc = new List<DataFeed>();
+            DateTime dateDebut = new DateTime(2010, 1, 1);
+            DateTime dateFin = new DateTime(2009, 1, 1);
+            Share action1 = new Share("accordId", "accordId");
+            VanillaCall vanille1 = new VanillaCall("accordId", action1, dateDebut, 10);
+            IDataFeedProvider data = new SimulatedDataFeedProvider();
+            dataFeedCalc = data.GetDataFeed(vanille1, dateFin);
+            Console.WriteLine(dataFeedCalc[0].Date);
+            Console.WriteLine(dataFeedCalc[0].PriceList.ToString());
+            decimal a = 10;
+            dataFeedCalc[50].PriceList.TryGetValue("accordId", out a);
+            Console.WriteLine(a);
+            Console.WriteLine("fin");
         }
     }
 }
