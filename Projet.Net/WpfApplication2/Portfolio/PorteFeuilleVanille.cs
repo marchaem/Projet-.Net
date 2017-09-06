6f6fc0fd4bcdbca46563b7cc8f70b0bf699e58d3
ensimag <ensimag@ENSIPC455>
2017-09-06 16:06:06 +0200

    balancement

--- Projet.Net/WpfApplication2/Portfolio/PorteFeuilleVanille.cs
+++ Projet.Net/WpfApplication2/Portfolio/PorteFeuilleVanille.cs
@@ -13,29 +13,44 @@ namespace WpfApplication2.Portfolio
         private OptionVanille option { get; set; }
 
 
-// proportions est un dictionnaire qui à une date donnée associe delta et (prix - delta)
-        private Dictionary<System.DateTime, double[]> proportions;
+        // proportions est matrice qui à une date (ligne ) associe les proportions (colonnes)
+        private double[,] proportions;
 
-        public PorteFeuilleVanille(OptionVanille option, Dictionary<System.DateTime,double[]> proportions)
+        public PorteFeuilleVanille(OptionVanille option, double[,] proportions)
         {
             this.option = option;
             this.proportions = proportions;
         }
-        public void actualisationPortef(DateTime date,double spot,double volatility)
+        public void actualisationPortef(DateTime date,double spot,double volatility,double r, double prixSousJ)
         {
-            double nbAction = option.calculDeltaVanille(date, 365, spot,  volatility);
-            double thuneSansrisque = 1; 
+            int a = fonctionkerboul(date);
+            double delta = option.calculDeltaVanille(date, 365, spot, volatility);
+            double thuneSansRisque = pricePortefeuille(date, prixSousJ, r, spot, volatility);
+            proportions[a, 0] = delta;
+            proportions[a, 1] = thuneSansRisque;
         }
         
-        public double pricePortefeuille(System.DateTime datePrec, double prixSousJ ,double r)
+        int fonctionkerboul(DateTime date)
         {
-            double[] proportion = new double[] { 0, 0 };
-            this.proportions.TryGetValue(datePrec, out proportion);
-            return proportion[0] * prixSousJ + proportion[1] * (1 + r); 
+            return 1;
+        }
+        public double pricePortefeuille(DateTime date , double prixSousJ ,double r,double spot, double volatility)
+        {
+            int a = fonctionkerboul(date);
+            if (a < 0)
+            {
+                throw new Exception("la fonction Kerboul a renvoyé un index négatif, c'est un boyard ");
+            }
+            if (a == 0)
+            {
+                return option.calculePrixVanille(date, 365, spot, volatility);
+            }
+            return proportions[a-1,0] * prixSousJ + proportions[a-1,1] * (1 + r); 
 
 
 
         }
+        /*
         public double getValeurActu(int nbPeriodes, double spot, double tauxSansRisque)
         {
             // spot = prix de l'action au bout de nbPeriodes
@@ -43,5 +58,6 @@ namespace WpfApplication2.Portfolio
             res = thuneDansLeSansRisque * Math.Pow(1 + tauxSansRisque, nbPeriodes) + spot * nombreAction;
             return res;
         }
+        */
     }
 }
