1a65f6f70c08555c09fce911b6ea953ffb49abc5
Marchal Emmanuel <emmanuel.marchalfrancis@gmail.com>
2017-09-10 19:38:19 +0200

    interface manu

---
+++ Projet.Net/WpfApplication2/ajoutBasket.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for ajoutBasket.xaml
+    /// </summary>
+    public partial class ajoutBasket : Window
+    {
+        public ajoutBasket()
+        {
+            InitializeComponent();
+          //  base.DataContext=
+        }
+
+        private void ajouter_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime maturité = Convert.ToDateTime(maturity.Text);
+            double strike = Convert.ToInt32(Strike.Text);
+            //ShareNames share =
+            this.Close();
+        }
+
+        private void annuler_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
